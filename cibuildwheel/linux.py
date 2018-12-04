0660f16f20f8b096b250d55eda04740ad8194eda
Will Rogers <wahrogers@gmail.com>
2018-12-04 15:50:46 +0000

    auditwheel changes

--- cibuildwheel/linux.py
+++ cibuildwheel/linux.py
@@ -78,7 +78,8 @@ def build(project_dir, output_dir, test_command, test_requires, before_build, bu
                     # pure python wheel - just copy
                     mv "$built_wheel" /tmp/delocated_wheel
                 else
-                    auditwheel show "$built_wheel" -w /tmp/delocated_wheel
+                    locate libc.so.6
+                    auditwheel show "$built_wheel"
                     auditwheel repair "$built_wheel" -w /tmp/delocated_wheel
                 fi
                 delocated_wheel=(/tmp/delocated_wheel/*.whl)
